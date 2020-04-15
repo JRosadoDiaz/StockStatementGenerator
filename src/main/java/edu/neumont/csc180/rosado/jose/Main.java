771357f0a84c52af54c2d3a394b1e9da1d2e0605
Neondangel <jdiaz@student.neumont.edu>
2020-04-15 15:42:27 -0600

    Built first test and classes to hold JSON data
    JSON can be read and parsed into a list of accounts

--- src/main/java/edu/neumont/csc180/rosado/jose/Main.java
+++ src/main/java/edu/neumont/csc180/rosado/jose/Main.java
@@ -1,10 +1,38 @@
 package edu.neumont.csc180.rosado.jose;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+
 public class Main {
+	
+	static String filePath = "C:\\Users\\Jose Rosado\\eclipse-workspace\\StockStatementGeneratorAssignment\\src\\main\\java\\edu\\neumont\\csc180\\rosado\\jose\\stock_transations.by.account.holder.json\\";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		JSONReader reader = new JSONReader(filePath);
+		
+//		try (OutputStream os = new FileOutputStream("out.pdf")) {
+//            PdfRendererBuilder builder = new PdfRendererBuilder();
+//            builder.useFastMode();
+//            builder.withUri("file:///Users/me/Documents/pdf/in.htm");
+//            builder.toStream(os);
+//            builder.run();
+//		}
 	}
 
 }
